248b7a5549bf81b8799f6023a917aad96c9f86e8
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 13:04:46 -0500

    elaboracion de metodo estatico dividir con mensaje

--- src/Taller_3_modificadorStatic/Ejercicio_2/Matematicas.java
+++ src/Taller_3_modificadorStatic/Ejercicio_2/Matematicas.java
@@ -12,4 +12,12 @@ public class Matematicas {
     public static double multiplicar(double a, double b) {
         return a * b;
     }
+
+    public static double dividir(double a, double b) {
+        if (b == 0) {
+            System.out.println("Error: No se puede dividir entre cero.");
+            return 0;
+        }
+        return a / b;
+    }
 }
