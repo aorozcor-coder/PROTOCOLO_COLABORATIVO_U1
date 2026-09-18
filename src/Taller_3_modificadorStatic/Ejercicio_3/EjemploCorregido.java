5541335a336b69e14b512e951a7a7b32573015b2
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 13:24:18 -0500

    elaboracion del codigo correcto y el codigo erroneo esta en forma de comentario

--- src/Taller_3_modificadorStatic/Ejercicio_3/EjemploCorregido.java
+++ src/Taller_3_modificadorStatic/Ejercicio_3/EjemploCorregido.java
@@ -1,10 +1,18 @@
 package Taller_3_modificadorStatic.Ejercicio_3;
-
+/*
 public class EjemploError {
     private String titulo = "POO"; // Atributo de instancia (no estático)
 
     public static void mostrarTitulo() {
         // ERROR DE COMPILACIÓN: non-static variable titulo cannot be referenced from a static context
+        System.out.println("Título: " + titulo);
+    }
+}
+*/
+public class EjemploCorregido {
+    private static String titulo = "POO"; // Atributo estático
+
+    public static void mostrarTitulo() {
         System.out.println("Título: " + titulo);
     }
 }
