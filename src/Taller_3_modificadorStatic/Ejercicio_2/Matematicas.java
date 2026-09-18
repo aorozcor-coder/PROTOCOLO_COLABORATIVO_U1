871b1532f2019fe84a136ec92ff1ace40844e013
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 13:02:58 -0500

    elaboracion de metodos para las operaciones exepto de division

---
+++ src/Taller_3_modificadorStatic/Ejercicio_2/Matematicas.java
@@ -0,0 +1,15 @@
+package Taller_3_modificadorStatic.Ejercicio_2;
+
+public class Matematicas {
+    public static double sumar(double a, double b) {
+        return a + b;
+    }
+
+    public static double restar(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiplicar(double a, double b) {
+        return a * b;
+    }
+}
