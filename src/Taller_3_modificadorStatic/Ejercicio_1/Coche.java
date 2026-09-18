d04061322f2c3bad9933ff57d665024c64fbac29
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 12:52:43 -0500

    elaboracion de clase Coche

--- src/Taller_3_modificadorStatic/Ejercicio_1/Coche.java
+++ src/Taller_3_modificadorStatic/Ejercicio_1/Coche.java
@@ -4,17 +4,4 @@ public class Coche {
     public String marca;
     public String modelo;
     public static int contadorCoches = 0;
-
-    // Constructor de la clase
-    public Coche(String marca, String modelo) {
-        this.marca = marca;
-        this.modelo = modelo;
-        contadorCoches++; // Se incrementa cada vez que se crea un nuevo Coche
-    }
-
-    // Método Statico
-    public static void mostrarContador() {
-        System.out.println("Total de coches creados: " + contadorCoches);
-
-    }
 }
