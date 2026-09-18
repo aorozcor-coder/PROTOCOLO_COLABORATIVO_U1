6383b9a17a76f57ac79ea41c4012cd9a881063c0
SamuelBolivarPina <sbolivarp1@unicartagena.edu.co>
2026-09-18 12:57:51 -0500

    elaboracion de constructores y carpetas para separar los trabajos

--- src/Taller_3_modificadorStatic/Ejercicio_1/Coche.java
+++ src/Taller_3_modificadorStatic/Ejercicio_1/Coche.java
@@ -4,4 +4,11 @@ public class Coche {
     public String marca;
     public String modelo;
     public static int contadorCoches = 0;
+
+    // Constructor de la clase
+    public Coche(String marca, String modelo) {
+        this.marca = marca;
+        this.modelo = modelo;
+        contadorCoches++; // Se incrementa cada vez que se crea un nuevo Coche
+    }
 }
